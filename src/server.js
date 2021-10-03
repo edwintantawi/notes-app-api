37d6853f43292a948d8a42111594377203caa156
edwintantawi <edwintantawi@gmail.com>
2021-10-03 13:43:50 +0700

    ✨ use notes plugin to server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
 
 const init = async () => {
+  const notesService = new NotesService();
+
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: { cors: { origin: ['*'] } },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: { service: notesService },
+  });
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
