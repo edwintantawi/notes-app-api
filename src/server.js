5b67437871a314904749771f6674b223cd43afb1
edwintantawi <edwintantawi@gmail.com>
2021-10-26 18:05:11 +0700

    aws s3

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
 const inert = require('@hapi/inert');
-const path = require('path');
+// const path = require('path');
 
 // notes
 const notes = require('./api/notes');
@@ -33,7 +33,8 @@ const ExportsValidator = require('./validator/exports');
 
 // uploads
 const uploads = require('./api/uploads');
-const StorageService = require('./services/storage/StorageService');
+// const StorageService = require('./services/storage/StorageService');
+const StorageService = require('./services/S3/StorageService');
 const UploadsValidator = require('./validator/uploads');
 
 // extensions
@@ -44,9 +45,10 @@ const init = async () => {
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
-  const storageService = new StorageService(
-    path.resolve(__dirname, 'api/uploads/file/images')
-  );
+  // const storageService = new StorageService(
+  //   path.resolve(__dirname, 'api/uploads/file/images')
+  // );
+  const storageService = new StorageService();
 
   const server = Hapi.server({
     port: process.env.PORT,
